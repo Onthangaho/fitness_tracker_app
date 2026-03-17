725b5e8a7ea5b86518d0288003a95ef472cea066
Onthangaho <onthangahomagoro@gmail.com>
2026-03-17 22:43:49 +0200

    feat:enhance Homescreen by adding a layoutbuilder to display different type of workouts with a favorite toogle button that change color when pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_tracker_app/workout_tile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fitness Tracker',
       debugShowCheckedModeBanner: false,
-      
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
@@ -30,10 +31,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Map<String, String>> workouts = [
+    {'name': 'Cardio', 'status': 'Completed'},
+    {'name': 'Strength Training', 'status': 'In Progress'},
+    {'name': 'Yoga Flow', 'status': 'Pending'},
+  ];
+  final List<IconData> workoutIcons = [
+    Icons.directions_run,
+    Icons.fitness_center,
+    Icons.self_improvement,
+  ];
+  final Set<int> favoriteWorkouts = {};
+
   @override
   Widget build(BuildContext context) {
     String? username;
-    
+
     String? displayName = username ?? 'Guest User';
     return Scaffold(
       appBar: AppBar(
@@ -44,64 +57,138 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        leading: Icon(Icons.fitness_center,),
+        leading: Icon(Icons.fitness_center),
         actions: [
-          IconButton(
-            onPressed: () {
-              
-            },
-            icon: Icon(Icons.account_circle),
-            
-          ),
-           Stack(
-            children:[ IconButton(
-              onPressed: () {
-                
-              },
-              icon: Icon(Icons.notifications),
-              
-            ),
-            Positioned(
-              top: 8,
-              right: 8,
-              child: Container(
-                padding: EdgeInsets.all(2),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  shape: BoxShape.circle,
-
-                ),
-                constraints: BoxConstraints(
-                  minWidth: 8,
-                  minHeight: 8,
-                ),
-                child: Text(
-                  '3',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
+          IconButton(onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Profile feature coming soon!'),
+              backgroundColor: Theme.of(context).colorScheme.primary,)
+            );
+          }, icon: Icon(Icons.account_circle)),
+          Stack(
+            children: [
+              IconButton(onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Notifications feature coming soon!'),
+                  backgroundColor: Theme.of(context).colorScheme.primary,)
+                );
+              }, icon: Icon(Icons.notifications)),
+              Positioned(
+                top: 8,
+                right: 8,
+                child: Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 1),
                   ),
-                  textAlign: TextAlign.center,
+                  constraints: BoxConstraints(minWidth: 10, minHeight: 10),
+                  child: Text(
+                    '3',
+                    style: TextStyle(color: Colors.white, fontSize: 10),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-               
               ),
-              
-
-            )
-            ]
+            ],
           ),
         ],
-        
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Welcome, $displayName',
+              style: Theme.of(
+                context,
+              ).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              'Ready to take your health Seriously?',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                color: Theme.of(context).colorScheme.onSurfaceVariant,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.onPrimary,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Theme.of(context).colorScheme.shadow.withValues(alpha:0.1 ),
+                    blurRadius: 6,
+                    offset: const Offset(0,2)
+                  )
+                ]
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 24,),
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  int crossAxisCount;
+                  if (constraints.maxWidth < 600) {
+                    crossAxisCount = 1;
+                  } else if (constraints.maxWidth < 900) {
+                    crossAxisCount = 2;
+                  } else {
+                    crossAxisCount = 3;
+                  }
 
-        child: Text("Welcome, ${displayName.toUpperCase()}! Ready to be fit?"),
+                  return GridView.builder(
+                    itemCount: workouts.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: crossAxisCount,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      childAspectRatio: 1.5,
+                    ),
+                    itemBuilder: (context, index) {
+                     final workout = workouts[index];
+
+                     return WorkoutTile(
+                      workoutName: workout['name']!,
+                      icon: workoutIcons[index],
+                      status: workout['status']!,
+                      isFavorite: favoriteWorkouts.contains(index),
+                      onFavoriteToggle: () {
+                        setState(() {
+                          if(favoriteWorkouts.contains(index)){
+                            favoriteWorkouts.remove(index);
+                          }else{
+                            favoriteWorkouts.add(index);
+                          }
+                        });
+                      },
+                     );
+                     
+                      
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
-        },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );

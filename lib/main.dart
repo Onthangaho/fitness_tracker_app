a14b6a59ded50f35ee2be676206665b5cc66ea06
Onthangaho <onthangahomagoro@gmail.com>
2026-03-17 11:34:59 +0200

    Refactor HomeScreen to be a StatefulWidget for dynamic user display

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     String? username;
